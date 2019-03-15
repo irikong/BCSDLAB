7bd06afe36bc81ed8adb2730d7ae785d0a398b34
irikong <gookirix@gmail.com>
2019-03-15 22:14:06 +0900

    Change lpszMenuName

--- WINAPI_TEST/WINAPI_TEST/WINAPI_TEST.cpp
+++ WINAPI_TEST/WINAPI_TEST/WINAPI_TEST.cpp
@@ -76,7 +76,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINAPITEST));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINAPITEST);
+	wcex.lpszMenuName   = NULL; //MAKEINTRESOURCEW(IDC_WINAPITEST);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
