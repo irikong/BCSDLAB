b7b4e342eaf1bd6e61743addfea1b6896ef75a07
irikong <gookirix@gmail.com>
2019-05-11 23:47:07 +0900

    Test C# Script

---
+++ Unity_Space_Shooter/Assets/Test/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int a = 10;
+    float b = 0.1f;
+    string c = "Unity";
+    bool d = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        Add();
+    }
+
+    void Add()
+    {
+        int result = 10 + 30;
+        print(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Update!!!!!");
+    }
+}
