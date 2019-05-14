5ced2c566271122eaf1e9f6509c1428b18b66f1e
irikong <gookirix@gmail.com>
2019-05-14 17:40:04 +0900

    Add Sound and Effect

---
+++ Unity_Space_Shooter/Assets/Sources/DestroyByContact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
